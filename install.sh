7fbff4d70c39248feb3e5855c5a9eadc2a4c70e3
galaxy-cli <kvnallbright@gmail.com>
2025-07-03 02:20:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-# Install all packages
-sudo apt install -y wget festival lame
+#!/bin/bash
+#                  _  __ _                                 _       _                         _                             _ _ 
+#        _ __   __| |/ _| |_ ___  _ __ ___  _ ____   _____| |__   | |__  _   _    __ _  __ _| | __ ___  ___   _        ___| (_)
+#       | '_ \ / _` | |_| __/ _ \| '_ ` _ \| '_ \ \ / / __| '_ \  | '_ \| | | |  / _` |/ _` | |/ _` \ \/ / | | |_____ / __| | |
+#       | |_) | (_| |  _| || (_) | | | | | | |_) \ V /\__ \ | | | | |_) | |_| | | (_| | (_| | | (_| |>  <| |_| |_____| (__| | |
+#       | .__/ \__,_|_|  \__\___/|_| |_| |_| .__/ \_(_)___/_| |_| |_.__/ \__, |  \__, |\__,_|_|\__,_/_/\_\\__, |      \___|_|_|
+#       |_|                                |_|                           |___/   |___/                    |___/
+# https://github.com/galaxey-cli/pdftompv
+# pdftompv.sh - PDF to MP3 converter utility using pdftotext, text2wave (Festival), LAME, and MPV
+
+# Updates repos and installs relevant packages for pdftompv.sh
+sudo apt install update
+sudo apt install -y poppler-utils wget festival lame
